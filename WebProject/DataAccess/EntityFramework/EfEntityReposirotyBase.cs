26f58f0c5847a247cf7949df5fbb5f2668ad90c4
oguz-kocer <68062849+oguz-kocer@users.noreply.github.com>
2022-05-24 19:10:26 +0300

    First Commit
    Proje oluşturuldu
    katmanlı mimari yapıldı

---
+++ WebProject/DataAccess/EntityFramework/EfEntityReposirotyBase.cs
@@ -0,0 +1,65 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.EntityFramework
+{
+	public class EfEntityRepositoryBase<TEntity, TContext> : IEntityRepository<TEntity>
+		where TEntity : class, IEntity, new()//TEntity class olmalı , IEntity türünde olmalı , newlenebilmeli.
+		where TContext : DbContext, new() //TContext DbContext türünde olmalı , newlenebilmeli 
+
+	{
+		public void Add(TEntity entity)
+		{
+			using (var context = new TContext())//using içindeki context garbage collector yardımıyla belleği hızlıca temizler.Performans için yazdık.
+			{
+				var addedEntity = context.Entry(entity);
+				addedEntity.State = EntityState.Added;//Ekleme işlemi yapılacağını bildirdik. 
+				context.SaveChanges();
+			}
+		}
+
+		public void Delete(TEntity entity)
+		{
+			using (var context = new TContext())
+			{
+				var deletedEntity = context.Entry(entity);
+				deletedEntity.State = EntityState.Deleted;
+				context.SaveChanges();
+			}
+		}
+
+		public TEntity Get(Expression<Func<TEntity, bool>> filter = null)
+		{
+			using (var context = new TContext())
+			{
+				return context.Set<TEntity>().SingleOrDefault(filter);
+			}
+		}
+
+		public List<TEntity> GetList(Expression<Func<TEntity, bool>> filter = null)
+		{
+			using (var context = new TContext())
+			{
+				return filter == null
+					? context.Set<TEntity>().ToList()
+					: context.Set<TEntity>().Where(filter).ToList();
+			}
+		}
+
+		public void Update(TEntity entity)
+		{
+			using (var context = new TContext())
+			{
+				var updatedEntity = context.Entry(entity);
+				updatedEntity.State = EntityState.Modified;
+				context.SaveChanges();
+			}
+		}
+	}
+}
---
+++ WebProject/Entities/Concrete/ProductCategory.cs
@@ -0,0 +1,36 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace Entities.Concrete
+{
+    /// <summary>
+    /// High-level product categorization.
+    /// </summary>
+    public partial class ProductCategory:IEntity
+    {
+        public ProductCategory()
+        {
+            ProductSubcategories = new HashSet<ProductSubcategory>();
+        }
+
+        /// <summary>
+        /// Primary key for ProductCategory records.
+        /// </summary>
+        public int ProductCategoryId { get; set; }
+        /// <summary>
+        /// Category description.
+        /// </summary>
+        public string Name { get; set; } = null!;
+        /// <summary>
+        /// ROWGUIDCOL number uniquely identifying the record. Used to support a merge replication sample.
+        /// </summary>
+        public Guid Rowguid { get; set; }
+        /// <summary>
+        /// Date and time the record was last updated.
+        /// </summary>
+        public DateTime ModifiedDate { get; set; }
+
+        public virtual ICollection<ProductSubcategory> ProductSubcategories { get; set; }
+    }
+}
